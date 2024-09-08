54e249c68acaf70d6fcdb7023e7600f88a2d395d
Deepak kumar <dee96872@gmail.com>
2024-09-08 11:37:53 +0530

    i add some home components and image

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,12 +1,80 @@
-import React from 'react'
-import "./TitleCards.css"
+// import React, { useEffect, useRef } from 'react'
+// import "./TitleCards.css"
+// import cards_data from '../../assets/cards/Cards_data'
 
-const TitleCards = () => {
+
+// const TitleCards = () => {
+//   const cardRef = useRef();
+
+//   const handleWheel = (event) =>{
+//     event.preventDefault;
+//     cardRef.current.scrollLeft += event.deltaY;
+//   }
+
+//   useEffect(() => {
+//     cardRef.current.addEventListener("wheel", handleWheel)
+//   }, [handleWheel])
+
+//   return (
+//     <div className='title-cards'>
+//       <h2>Popular on Netfilex</h2>
+//       <div className="card-list" ref={cardRef}>
+//         {
+//           cards_data.map((card, index) => {
+//             return <div className="card" key={index}>
+//               <img src={card.image} alt="" />
+//               <p>{card.name}</p>
+//             </div>
+//           })
+//         }
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default TitleCards
+
+
+
+
+import React, { useEffect, useRef } from 'react';
+import './TitleCards.css';
+import cards_data from '../../assets/cards/Cards_data';
+
+const TitleCards = ({title, category}) => {
+  const cardRef = useRef();
+
+  const handleWheel = (event) => {
+    event.preventDefault(); // Invoke preventDefault correctly
+    cardRef.current.scrollLeft += event.deltaY;
+  };
+
+  useEffect(() => {
+    const cardListRef = cardRef.current;
+
+    // Add event listener
+    cardListRef.addEventListener('wheel', handleWheel);
+
+    // Clean up event listener on unmount
+    return () => {
+      cardListRef.removeEventListener('wheel', handleWheel);
+    };
+  }, []);
+
   return (
-    <div className='titlecards'>
-      
+    <div className="title-cards">
+      <h2>{title?title:"Popular on Netfilex"}</h2>
+      <div className="card-list" ref={cardRef}>
+        {cards_data.map((card, index) => (
+          <div className="card" key={index}>
+            <img src={card.image} alt={card.name} />
+            <p>{card.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TitleCards
+export default TitleCards;
+
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
-import "./Home.css"
-import Navbar from '../../components/Navbar/Navbar'
+import React from 'react';
+import './Home.css';
+import Navbar from '../../components/Navbar/Navbar';
+import hero_banner from '../../assets/hero_banner.jpg';
+import hero_title from '../../assets/hero_title.png';
+import play_icon from '../../assets/Play_icon.png';
+import info_icon from '../../assets/info_icon.png';
+import TitleCards from '../../components/TitleCards/TitleCards';
 
 function Home() {
   return (
-    <div className='home'>
-      <Navbar/>
+    <div className="home">
+      <Navbar />
+      <div className="hero">
+        <img src={hero_banner} alt="Hero Banner" className="banner-img" />
+        <div className="hero-caption">
+          <img src={hero_title} alt="Hero Title" className="caption-img" />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae eaque similique officia sint et cumque ratione officiis rem beatae eveniet quas nisi iste sapiente doloremque, est nulla tempora autem, in, magnam sed ipsam sit?
+          </p>
+          <div className="hero-btns">
+            <button className="btn">
+              <img src={play_icon} alt="Play Icon" /> Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={info_icon} alt="Info Icon" /> More Info
+            </button>
+          </div>
+          <TitleCards />
+        </div>
+      </div>
+      <div className="more-cards">
+        <TitleCards title={"Blockbuster Movies"} />
+        <TitleCards title={"Only on Netflix"} />
+        <TitleCards title={"Upcoming"} />
+        <TitleCards title={"Top Pics for You"} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
